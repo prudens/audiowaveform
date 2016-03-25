347358dbabe79fa1f856fe78be994bde0daf1484
prudens <zhangnaigan_1989@163.com>
2016-03-25 20:41:47 +0800

    fix bug

--- src/asyntask.h
+++ src/asyntask.h
@@ -13,187 +13,201 @@
 #else
 #define TRACE(...) printf(__VA_ARGS__);
 #endif
-namespace prudens
+namespace snail
 {
-    uint64_t now()
+    namespace audio
     {
-        return duration_cast<milliseconds>(
-            system_clock::now() - time_point<system_clock>() ).count();
-    }
-    typedef  std::function<void( uint32_t ) > TaskExecute;
-    struct Task
-    {
+        using namespace  std::chrono;
+        using namespace std::placeholders;
+        uint64_t now()
+        {
+            return duration_cast<milliseconds>(
+                system_clock::now() - time_point<system_clock>() ).count();
+        }
+        typedef  std::function<void( uint32_t ) > TaskExecute;
+        struct Task
+        {
 #define MAX_MACRO_COMPILE_SUPPORT
-        uint32_t id;
-        uint32_t elapsed;
-        uint64_t expired;
-        bool repeat;
-        TaskExecute execute;
-        Task() :id( 0 ),
-            elapsed( 0 ),
-            expired( std::numeric_limits<uint64_t>::max MAX_MACRO_COMPILE_SUPPORT() ),
-            repeat( false )
-        {
-        }
-
-        bool operator < ( const Task& other )const
-        {
-            return this->elapsed < other.elapsed;
-        }
-    };
-    class AsynTask
-    {
-    public:
-        class TaskQueue
-        {
-        public:
-            TaskQueue() {
-                _stop.store( false );
-                _flag.store( false );
+            uint32_t id;
+            uint32_t elapsed;
+            uint64_t expired;
+            bool repeat;
+            TaskExecute execute;
+            Task() :id( 0 ),
+                elapsed( 0 ),
+                expired( std::numeric_limits<uint64_t>::max MAX_MACRO_COMPILE_SUPPORT() ),
+                repeat( false )
+            {
             }
 
-            void Run()
+            bool operator < ( const Task& other )const
             {
-                while ( true )
+                return this->elapsed < other.elapsed;
+            }
+        };
+        class AsynTask
+        {
+        public:
+            class TaskQueue
+            {
+            public:
+                TaskQueue()
+                {
+                    _stop.store( false );
+                    _flag.store( false );
+                }
+                ~TaskQueue()
+                {
+                    stop();
+                }
+                void Run()
                 {
                     while ( true )
                     {
-                        if (_stop)
+                        while ( true )
                         {
-                            return;
-                        }
-                        Task task;
-                        uint64_t t = now();
-                        {
-                            std::lock_guard<std::mutex> guard( _lock );
-                            if ( !_tasks.empty() )
+                            if ( _stop )
                             {
-                                task = _tasks.top();
+                                return;
                             }
-                        }
-                        if ( task.expired >= t )
-                        {
-                            break;
-                        }
-                        if ( task.execute )
-                        {
-                            task.execute( task.id );
-                        }
-                        {
-                            std::lock_guard<std::mutex> guard( _lock );
-                            if ( !_tasks.empty() )
+                            Task task;
+                            uint64_t t = now();
                             {
-                                _tasks.pop();
-                                if ( task.repeat )
+                                std::lock_guard<std::mutex> guard( _lock );
+                                if ( !_tasks.empty() )
                                 {
-                                    task.expired = task.elapsed + now();
-                                    _tasks.push( task );
+                                    task = _tasks.top();
+                                }
+                                if ( task.expired < t )
+                                {
+                                    _tasks.pop();
+                                    if ( task.repeat )
+                                    {
+                                        task.expired = task.elapsed + t;
+                                        _tasks.push( task );
+                                    }
+                                }
+                                else
+                                {
+                                    break;
                                 }
                             }
 
+                            if ( task.execute )
+                            {
+                                task.execute( task.id );
+                            }
                         }
-                    }
-                    int32_t sleep_time = std::numeric_limits<int32_t>::max MAX_MACRO_COMPILE_SUPPORT ();//列表为空的时候就睡眠30ms
-                    {
-                        std::lock_guard<std::mutex> guard( _lock );
+                        if ( _stop )
+                        {
+                            return;
+                        }
+                        int32_t sleep_time = std::numeric_limits<int32_t>::max MAX_MACRO_COMPILE_SUPPORT();//列表为空的时候就睡眠30ms
+
+                        std::unique_lock <std::mutex> lck( _lock );
                         if ( !_tasks.empty() )
                         {
-                            sleep_time = static_cast<int32_t>(_tasks.top().expired - now());
-                            if (sleep_time<0)
+                            sleep_time = static_cast<int32_t>( _tasks.top().expired - now() );
+                            if ( sleep_time < 0 )
                             {
                                 continue;
                             }
                         }
+                        if ( _flag.load() == false )
+                        {
+                            _cond_variable.wait_for( lck, milliseconds( sleep_time ),
+                                                     [this] ()->bool { return this->_flag.load(); } );
+                        }
+                        if ( _stop )
+                        {
+                            return;
+                        }
+                        _flag.store( false );
                     }
-                    if (_stop)
-                    {
-                        return;
-                    }
+                }
+
+                void push( Task& task )
+                {
                     std::unique_lock <std::mutex> lck( _lock );
-                    if ( _flag.load() == false )
+                    _tasks.push( task );
+                    _flag.store( true );
+                    _cond_variable.notify_all();
+
+                }
+
+                void clear()
+                {
+                    std::lock_guard<std::mutex> guard( _lock );
+                    while ( !_tasks.empty() )
                     {
-                        _cond_variable.wait_for( lck, milliseconds( sleep_time ),
-                                                 [this] ()->bool { return this->_flag.load(); } );
+                        _tasks.pop();
                     }
-
-                    _flag.store( false );
                 }
-            }
 
-            void push( Task& task )
-            {
-                std::unique_lock <std::mutex> lck( _lock );
-                _tasks.push( task );
-                _flag.store( true );
-                _cond_variable.notify_all();
-                
-            }
-
-            void clear()
-            {
-                std::lock_guard<std::mutex> guard( _lock );
-                while ( !_tasks.empty() )
+                void remove( uint32_t task_id )
                 {
-                    _tasks.pop();
+                    std::lock_guard<std::mutex> guard( _lock );
+                    _tasks.remove_if( [&] ( const Task& task )
+                    {
+                        return task.id == task_id;
+                    } );
                 }
-            }
 
-            void remove( uint32_t task_id )
-            {
-                std::lock_guard<std::mutex> guard( _lock );
-                _tasks.remove_if( [&] ( const Task& task )
+                void stop()
                 {
-                    return task.id == task_id;
-                } );
+                    std::unique_lock <std::mutex> lck( _lock );
+                    _flag.store( true );
+                    _stop.store( true );
+                    _cond_variable.notify_all();
+                }
+            private:
+                std::atomic_bool _stop;
+                max_min_heap<Task> _tasks;
+                std::mutex _lock;
+                std::condition_variable _cond_variable;
+                std::atomic_bool _flag;
+            };
+
+            AsynTask()
+            {
+                _task_queue = std::make_shared<TaskQueue>();
+                std::thread( [] ( std::shared_ptr<TaskQueue> queue )
+                {
+                    queue->Run();
+                }, _task_queue ).detach();
             }
 
+            ~AsynTask()
+            {
+                _task_queue->stop();
+            }
+
+            uint32_t PostTask( int elapsed_ms, bool repeat, TaskExecute execute )
+            {
+                Task task;
+                task.elapsed = elapsed_ms;
+                task.expired = elapsed_ms + now();
+                task.repeat = repeat;
+                task.id = s_task_id++;
+                task.execute = execute;
+                _task_queue->push( task );
+                return task.id;
+            }
+
+            void RemoveTask( uint32_t task_id )
+            {
+                _task_queue->remove( task_id );
+            }
+
+            void RemoveAllTask()
+            {
+                _task_queue->clear();
+            }
         private:
-            std::atomic_bool _stop;
-            prudens::max_min_heap<Task> _tasks;
-            std::mutex _lock;
-            std::condition_variable _cond_variable;
-            std::atomic_bool _flag;
+
+            std::shared_ptr<TaskQueue> _task_queue;
+            static uint32_t s_task_id;
         };
-
-        AsynTask()
-        {
-            _task_queue = std::make_shared<TaskQueue>();
-            std::thread( [] ( std::shared_ptr<TaskQueue> queue )
-            {
-                queue->Run();
-            },_task_queue ).detach();
-        }
-
-        ~AsynTask()
-        {
-            _task_queue->clear();
-        }
-
-        uint32_t PostTask( int elapsed_ms, bool repeat, TaskExecute execute )
-        {
-            Task task;
-            task.elapsed = elapsed_ms ;
-            task.expired = elapsed_ms + now();
-            task.repeat = repeat;
-            task.id = s_task_id++;
-            task.execute = execute;
-            _task_queue->push( task );
-            return task.id;
-        }
-
-        void RemoveTask( uint32_t task_id )
-        {
-            _task_queue->remove( task_id );
-        }
-
-        void RemoveAllTask()
-        {
-            _task_queue->clear();
-        }
-    private:
-        
-        std::shared_ptr<TaskQueue> _task_queue;
-        static uint32_t s_task_id;
-    };
-    uint32_t AsynTask::s_task_id = 1;
+        uint32_t AsynTask::s_task_id = 1;
+    }
 }
